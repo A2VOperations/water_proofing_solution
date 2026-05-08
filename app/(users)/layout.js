7bb595aa443d4d910327427cfdc9f9d5932f9279
A2VOperations <operation.a2vgroups@gmail.com>
2026-05-08 11:10:09 +0530

    done changes

--- app/(users)/layout.js
+++ app/(users)/layout.js
@@ -32,7 +32,7 @@ export const metadata = {
 };
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className="min-h-full flex flex-col">
         <Navbar />
         {children}
